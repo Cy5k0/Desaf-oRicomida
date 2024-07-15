29bbb177bed3948b92f50b8ea7956a321ee05afa
Cy5k0 <francisco.colomer@gmail.com>
2024-07-15 14:55:08 -0400

    c final

---
+++ assets/js/scripts.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', function () {
+    //carousel
+
+    var myCarousel = new bootstrap.Carousel(document.getElementById('#carouselPlatos'), {
+        interval: false 
+    });
+
+
+    //tooltips
+    
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl)
+    })
+
+
+});
+
+$(document).ready(function() {
+    $("#enviarCorreo").click(function() {
+        alert("El correo fue enviado correctamente...");
+    });
+
+    $('h3').on('dblclick', function() {
+        if ($(this).text() === "INGREDIENTES" || $(this).text() === "PREPARACIÓN") {
+            $(this).toggleClass('red-text');
+        }
+    });
+
+    $('.recipe-card .card-title').on('click', function() {
+        $(this).siblings('.card-content').toggle();
+    });
+
+});
